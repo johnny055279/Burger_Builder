bd090930b1f8cecf6b1dc05dee3bc259c26f038f
johnny055279 <johnny055279@gmail.com>
2020-03-03 15:49:27 +0800

    Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Button from '../../UI/Button/Button'
 
-const orderSummery = (props) => {
+class orderSummery extends Component {
 
-    let ingredientSummary = Object.keys(props.ingredients).map(ingredientsKey => {
-    return <li key={ingredientsKey}>
-            <span style = {{textTransform: 'capitalize'}}>{ingredientsKey}</span>: {props.ingredients[ingredientsKey]}
-           </li>
-    });
+    // By using componentWillUpdate we see even the orderSummery not show in the page, it still rerender when click any button.
+    // So we chould improve performance by editing [Modal] because it is unnecessary to update.
+    componentWillUpdate() {
+        console.log('[orderSummery] will update');
+    }
 
-    return(
-        <>
-        <h3>Your Oder</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>
-            {ingredientSummary}
-        </ul>
-    <p><strong>Total Price: {props.price}</strong></p>
-        <p>Continue to checkout?</p>
+    render() {
 
-        {/* Make sure the buttonType value is same as Button.module.css name*/}
-        <Button clicked = {props.purchaseCancelled} buttonType = 'Danger'>Cancel</Button>
-        <Button clicked = {props.continuePurchasing} buttonType = 'Success'>Continue</Button>
+        let ingredientSummary = Object.keys(this.props.ingredients).map(ingredientsKey => {
+            return <li key={ingredientsKey}>
+                    <span style = {{textTransform: 'capitalize'}}>{ingredientsKey}</span>: {this.props.ingredients[ingredientsKey]}
+                   </li>
+            });
 
-        </>
-    );
+        return(
+            <>
+            <h3>Your Oder</h3>
+            <p>A delicious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+        <p><strong>Total Price: {this.props.price}</strong></p>
+            <p>Continue to checkout?</p>
+    
+            {/* Make sure the buttonType value is same as Button.module.css name*/}
+            <Button clicked = {this.props.purchaseCancelled} buttonType = 'Danger'>Cancel</Button>
+            <Button clicked = {this.props.continuePurchasing} buttonType = 'Success'>Continue</Button>
+    
+            </>
+        );
+    }
 };
 
 export default orderSummery
